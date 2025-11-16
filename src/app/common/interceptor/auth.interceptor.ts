502c46f26ab4cfd3e437ad16611bb5406d109ed1
Duc <ducvt1310E@gmail.com>
2025-11-16 16:38:01 +0700

    #add error handling and token navigation in auth interceptor

--- src/app/common/interceptor/auth.interceptor.ts
+++ src/app/common/interceptor/auth.interceptor.ts
@@ -1,11 +1,18 @@
 import { HttpInterceptorFn } from '@angular/common/http';
 import { inject } from '@angular/core';
+import { Router } from '@angular/router';
+import { catchError, throwError } from 'rxjs';
 import { environment } from '../../../environments/environment.development';
 import { API } from '../../shared/const.api';
+import { ROUTE } from '../../shared/const.route';
 import { LocalStorage } from '../service/local-storage';
+import { NzMessageService } from 'ng-zorro-antd/message';
 
 export const authInterceptor: HttpInterceptorFn = (req, next) => {
   const localStorageService = inject(LocalStorage);
+  const router = inject(Router);
+  const messageService = inject(NzMessageService);
+
   const authToken = localStorageService.getAuthToken();
   const requestUrl = environment.api + req.url;
 
@@ -21,5 +28,17 @@ export const authInterceptor: HttpInterceptorFn = (req, next) => {
 
   req = req.clone({ url: requestUrl });
 
-  return next(req);
+  return next(req).pipe(
+    catchError((error) => {
+      const tokenErrorMessages = ['Invalid token', 'Token expired'];
+
+      if (tokenErrorMessages.includes(error.error.message)) {
+        router.navigate([ROUTE.AUTH.LOGIN]);
+      }
+
+      messageService.error(error.error.message, {});
+
+      return throwError(() => error);
+    })
+  );
 };
